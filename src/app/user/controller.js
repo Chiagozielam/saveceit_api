4797ae4b51bd1fa2097cd6b48ece2a0df1751951
Daniel Don <dandons25@gmail.com>
2019-12-06 16:38:14 +0100

    comforming to eslint

--- src/app/user/controller.js
+++ src/app/user/controller.js
@@ -2,8 +2,8 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 import cloudinary from "cloudinary";
-import { Receipt, Profile, User } from "../../database";
 import _ from "lodash";
+import { Receipt, Profile, User } from "../../database";
 import { registerValidation, loginValidation } from "./validation";
 import app from "../..";
 import { rejects } from "assert";
@@ -16,16 +16,16 @@ const userRegister = async (req, res) => {
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  //Check if user already exists
+  // Check if user already exists
   const emailExists = await User.findOne({ email: req.body.email });
-  if (emailExists) return res.status(400).send("Email already exists");
+  if (emailExists) return res.status(400).send('Email already exists');
 
-  //Hash Password
+  // Hash Password
   const salt = await bcrypt.genSalt(10);
   const hashPassword = await bcrypt.hash(req.body.password, salt);
 
   // Create new User
-  const { firstname, lastname, email, phoneNumber, password } = req.body;
+  const { firstname, lastname, email, phoneNumber } = req.body;
   const newUser = new User({
     firstname,
     lastname,
@@ -36,7 +36,7 @@ const userRegister = async (req, res) => {
   try {
     const saveUser = await newUser.save();
     if (saveUser) {
-      //Create and assign a token
+      // Create and assign a token
       const token = jwt.sign(
         { _id: saveUser._id, email: saveUser.email },
         process.env.USER_TOKEN_SECRET
@@ -92,10 +92,10 @@ const addReceipts = async (req, res) => {
   const cloud_name = "dcft8yhab";
   const api_key = "952961694399734";
   const api_secret = "iRlt9ZFuucvVBAIrqZYpitijsDY";
-  
-  const saveceitUserId = req.user._id
-  const receiptName = req.body.receiptName
-  console.log(receiptName)
+
+  const saveceitUserId = req.user._id;
+  const receiptName = req.body.receiptName;
+  console.log(receiptName);
   let imgArray = [];
 
   const { ...rest } = req.files;
@@ -111,16 +111,16 @@ const addReceipts = async (req, res) => {
   console.log(files);
   // let filePaths = imgFilePaths;
   const multiUpload = async () => {
-    for(let i=0; i <= keyLength - 1; i ++){
+    for (let i = 0; i <= keyLength - 1; i++) {
       let file = files[i];
-      console.log(file)
+      console.log(file);
       const path = `${__dirname}/../../temp/${file.name}`;
 
       //temporary store file on the server
       file.mv(path, err => {
-        if(err){
-          console.log(`error moving file: ${err}`)
-          return
+        if (err) {
+          console.log(`error moving file: ${err}`);
+          return;
         }
       });
       // upload files to cloudinary
@@ -129,37 +129,37 @@ const addReceipts = async (req, res) => {
         folder: `/receipts/`,
         use_filename: true
       });
-      await res.then( data => {
-        console.log(`success uploading files to cloudinary`)
-        // remove stored file and return upload result
-        fs.unlink(path, err => {
-          if (err) return reject(err);
-        });
-        const imgUrl = data.secure_url
-        imgArray = [...imgArray, imgUrl]
-      }).catch( err => console.log(err))
+      await res
+        .then(data => {
+          console.log(`success uploading files to cloudinary`);
+          // remove stored file and return upload result
+          fs.unlink(path, err => {
+            if (err) return reject(err);
+          });
+          const imgUrl = data.secure_url;
+          imgArray = [...imgArray, imgUrl];
+        })
+        .catch(err => console.log(err));
     }
     const receiptFile = {
       receiptName,
       recieptImg: imgArray,
       owner: saveceitUserId
-    }
-    console.log(receiptFile)
-    const newReceipt = new Receipt(receiptFile)
+    };
+    console.log(receiptFile);
+    const newReceipt = new Receipt(receiptFile);
     console.log(newReceipt);
-    try{
+    try {
       const saveReceipt = await newReceipt.save();
-      if(saveReceipt){
-        console.log("New Receipt has successfully been saved")
+      if (saveReceipt) {
+        console.log("New Receipt has successfully been saved");
         res.send("New Receipt has successfully been saved");
       }
-    }catch(err){
-      console.log("Unsuccessful saving receipt to database")
+    } catch (err) {
+      console.log("Unsuccessful saving receipt to database");
     }
-
-  }
-  multiUpload()
-  
+  };
+  multiUpload();
 };
 
 // -----------THIS SECTION IS FOR VIEWNIG ALL PRODUCTS ADDED BY A USER-----------------
