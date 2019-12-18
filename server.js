d173bc1b0613f31af8faad2461d0a7aed6a650a2
Daniel Don <dandons25@gmail.com>
2019-12-19 00:33:24 +0100

    add consola

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import consola from 'consola';
 import app from './src';
 
 
@@ -8,5 +9,5 @@ app.use(cors({ origin: '*' }));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`the server has been started on port ${5000}`);
+  consola.log(`the server has been started on port ${5000}`);
 });
